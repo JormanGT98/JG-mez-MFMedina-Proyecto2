2c8318c7bd46ee1a6a4553e1d7802fcb52461df5
JormanGT98 <48070857+JormanGT98@users.noreply.github.com>
2019-06-11 11:59:59 -0600

    Add files via upload

--- Proyecto2.py
+++ Proyecto2.py
@@ -9,14 +9,14 @@ import threading
 import time
 from os import path
 from random import randint
-def thread_function():
-    while True and firstime == False:
-        message, address = sock.recvfrom(255)
-        values = pickle.loads(message)
-        if(values[0] == "puntajes"):
-            listbox.insert(END, values[1])
-        elif values[0] != nam:
-            print(values)
+#def thread_function():
+ #   while True and firstime == False:
+  #      message, address = sock.recvfrom(255)
+   #     values = pickle.loads(message)
+    #    if(values[0] == "puntajes"):
+     #       listbox.insert(END, values[1])
+      #  elif values[0] != nam:
+       #     print(values)
 def destruirMenu():
     Menu.destroy()
     canvaElegirJ()
@@ -24,28 +24,32 @@ def destruirElegirJ():
     ElegirJ.destroy()
     canvaMenu()
 def IrAlJuego():
-    add()
-    nombre=entrada.get()
+    #add()
+    nombre1=entrada1.get()
+    nombre2=entrada2.get()
     ElegirJ.destroy()
     Main.iconify()
-    Juego(nombre)
-def add():
-    values=[entrada.get(), btnSiguiente.cget('text')] 
-    data=pickle.dumps(values)
-    sock.sendto(data, (multicast_addr, port))    
+    Juego(nombre1,nombre2)
+#def add():
+    #values=[entrada.get(), btnSiguiente.cget('text')] 
+    #data=pickle.dumps(values)
+    #sock.sendto(data, (multicast_addr, port))    
 
 def canvaElegirJ():
     global ElegirJ
-    global entrada
+    global entrada1
+    global entrada2
     global btnSiguiente
     ElegirJ=tk.Canvas(Main, height = 700, width = 600,bg="DarkGoldenrod2")
     ElegirJ.pack(fill=tk.BOTH, expand=True)    
-    lblNuevoJugador=tk.Label(ElegirJ,text="Si eres un nuevo Jugador, ingresa tu nombre aquí",bg="DarkGoldenrod2",fg="white",bd=0,font=("Times", 18))
-    lblNuevoJugador.pack(side=tk.TOP)
-    entrada=tk.Entry(ElegirJ)
-    entrada.pack(side=tk.TOP)
-    lblCargarJugador=tk.Label(ElegirJ,text="Si ya habias jugado antes, busca tu nombre aquí",bg="DarkGoldenrod2",fg="white",bd=0,font=("Times", 18))
-    lblCargarJugador.pack(side=tk.TOP)
+    lblJugador1=tk.Label(ElegirJ,text="Ingresa el nombre del Jugador1",bg="DarkGoldenrod2",fg="white",bd=0,font=("Times", 18))
+    lblJugador1.pack(side=tk.TOP)
+    entrada1=tk.Entry(ElegirJ)
+    entrada1.pack(side=tk.TOP)
+    lblJugador2=tk.Label(ElegirJ,text="Ingresa el nombre del Jugador2",bg="DarkGoldenrod2",fg="white",bd=0,font=("Times", 18))
+    lblJugador2.pack(side=tk.TOP)
+    entrada2=tk.Entry(ElegirJ)
+    entrada2.pack(side=tk.TOP)
     btnSiguiente=tk.Button(ElegirJ,text="Siguiente",bg="DarkGoldenrod2",fg="white",bd=0,command=IrAlJuego,font=("Times", 18))
     btnSiguiente.pack(side=tk.TOP)
     btnVolver=tk.Button(ElegirJ,text="Volver al Menu Principal",bg="DarkGoldenrod2",fg="white",bd=0,command=destruirElegirJ,font=("Times", 18))
@@ -64,7 +68,7 @@ def canvaMenu():
     SalirBtn=tk.Button(Menu,text="Salir",bg="DarkGoldenrod2",fg="White",font=("Times", 24),command=Main.destroy,bd=0)
     SalirBtn.pack(side=tk.TOP)
     Main.mainloop()
-def Juego(nombre):
+def Juego(nombre1,nombre2):
     pygame.init()
     #Constantes de el color de fondo y el tipo de letra que se va a usar
     letra=pygame.font.Font(None,20)
@@ -72,15 +76,23 @@ def Juego(nombre):
     #Constantes del tamaño de la pantalla y el titulo
     win= pygame.display.set_mode((500,500))
     pygame.display.set_caption("pyDakarDeath")
-    #Constantes y Variables del vehiculo y Jugador
+    #Constantes y Variables del vehiculo1 y Jugador1
     Carro1=pygame.image.load("car1.png") 
-    Jugador=Carro1.get_rect()
-    Jugador.centerx=500/2
-    Jugador.centery=450
+    Jugador1=Carro1.get_rect()
+    Jugador1.centerx=200
+    Jugador1.centery=450
     velocidad=10
-    choca=False
-    chocaPiedra=False
-    perder=0
+    chocaJugador1=False
+    chocaPiedraJugador1=False
+    perderJugador1=0
+    #Constantes y variables del vehiculo2 y jugador2
+    Carro2=pygame.image.load("car2.png")
+    Jugador2=Carro2.get_rect()
+    Jugador2.centerx=400
+    Jugador2.centery=450
+    chocaJugador2=False
+    chocaPiedraJugador2=False
+    perderJugador2=0
     #Constantes y variables de el obstaculo del cactus
     Cactus1=pygame.image.load("Cactus1.png")
     Obstaculo1=Cactus1.get_rect()
@@ -95,26 +107,35 @@ def Juego(nombre):
     pygame.mixer.music.play(4)
     Choque=pygame.mixer.Sound("Choque.wav")
     #Variable que va a mostrar la puntuacion de la pantalla, se modifica dentro del while
-    punt=0
+    puntJugador1=0
+    puntJugador2=0
     #Variable que se mantiene True hasta que se termine el programa y ocasiona que se termine el while
     run=True  
     #Loop que hace que se mantenga actualizandose la pantalla
     while run:        
-        puntuacion=letra.render(str(nombre)+":"+str(punt), 0,(0,0,0))
+        puntuacionJugador1=letra.render(str(nombre1)+":"+str(puntJugador1), 0,(0,0,0))
+        puntuacionJugador2=letra.render(str(nombre2)+":"+str(puntJugador2), 0,(0,0,0))
         pygame.time.delay(100)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
         keys= pygame.key.get_pressed()    
-        if keys[pygame.K_LEFT]and Jugador.left>0+velocidad:
-            Jugador.centerx-=velocidad
-        if keys[pygame.K_RIGHT]and Jugador.right<500-velocidad:
-            Jugador.centerx+=velocidad
-        if keys[pygame.K_UP]and Jugador.top>0+velocidad:
-            Jugador.centery-=velocidad
-        if keys[pygame.K_DOWN]and Jugador.bottom<500-velocidad:
-            Jugador.centery+=velocidad
-
+        if keys[pygame.K_LEFT]and Jugador1.left>0+velocidad:
+            Jugador1.centerx-=velocidad
+        if keys[pygame.K_RIGHT]and Jugador1.right<500-velocidad:
+            Jugador1.centerx+=velocidad
+        if keys[pygame.K_UP]and Jugador1.top>0+velocidad:
+            Jugador1.centery-=velocidad
+        if keys[pygame.K_DOWN]and Jugador1.bottom<500-velocidad:
+            Jugador1.centery+=velocidad
+        if keys[pygame.K_a]and Jugador2.left>0+velocidad:
+            Jugador2.centerx-=velocidad
+        if keys[pygame.K_d]and Jugador2.right<500-velocidad:
+            Jugador2.centerx+=velocidad
+        if keys[pygame.K_w]and Jugador2.top>0+velocidad:
+            Jugador2.centery-=velocidad
+        if keys[pygame.K_s]and Jugador2.bottom<500-velocidad:
+            Jugador2.centery+=velocidad
             
         if(Obstaculo1.centery>=550):
             aparece=False
@@ -136,31 +157,52 @@ def Juego(nombre):
             Obstaculo2.centery+=velocidadObst
 
             
-        if Jugador.colliderect(Obstaculo1)and choca==False:
-            punt-=200
-            choca=True
+        if Jugador1.colliderect(Obstaculo1)and chocaJugador1==False:
+            puntJugador1-=200
+            chocaJugador1=True
             Choque.play()
-        elif(choca==True and Jugador.colliderect(Obstaculo1)!= True):
+        elif(chocaJugador1==True and Jugador1.colliderect(Obstaculo1)!= True):
             choca=False
-            perder+=1
+            perderJugador1+=1
+
+        if Jugador2.colliderect(Obstaculo1)and chocaJugador2==False:
+            puntJugador2-=200
+            chocaJugador2=True
+            Choque.play()
+        elif(chocaJugador2==True and Jugador.colliderect(Obstaculo1)!= True):
+            chocaJugador2=False
+            perderJugador2+=1
 
             
-        if Jugador.colliderect(Obstaculo2)and chocaPiedra==False:
-            punt-=200
-            chocaPiedra=True
+        if Jugador1.colliderect(Obstaculo2)and chocaPiedraJugador1==False:
+            puntJugador1-=200
+            chocaPiedraJugador1=True
             Choque.play()
-        elif(chocaPiedra==True and Jugador.colliderect(Obstaculo2)!= True):
-            chocaPiedra=False
-            perder+=1
+        elif(chocaPiedraJugador1==True and Jugador1.colliderect(Obstaculo2)!= True):
+            chocaPiedraJugador1=False
+            perderJugador1+=1
+
             
-        if(perder==5):
+        if Jugador2.colliderect(Obstaculo2)and chocaPiedraJugador2==False:
+            puntJugador2-=200
+            chocaPiedraJugador2=True
+            Choque.play()
+        elif(chocaPiedraJugador2==True and Jugador2.colliderect(Obstaculo2)!= True):
+            chocaPiedraJugador2=False
+            perderJugador2+=1    
+        if(perderJugador1==5):
             run=False
-        punt+=5
+        if( perderJugador2==5):
+            run=False
+        puntJugador1+=5
+        puntJugador2+=5
         win.fill(colorFondo)
         win.blit(Piedra,Obstaculo2)
         win.blit(Cactus1,Obstaculo1)
-        win.blit(puntuacion,(0,0))
-        win.blit(Carro1,Jugador)
+        win.blit(puntuacionJugador1,(0,0))
+        win.blit(puntuacionJugador2,(0,20))
+        win.blit(Carro1,Jugador1)
+        win.blit(Carro2,Jugador2)
         pygame.display.flip()
     pygame.quit()
     Main.deiconify()
@@ -181,9 +223,9 @@ PuntajeBtn.pack(side=tk.TOP)
 SalirBtn=tk.Button(Menu,text="Salir",bg="DarkGoldenrod2",fg="White",font=("Times", 24),command=Main.destroy,bd=0)
 SalirBtn.pack(side=tk.TOP)
 #Se maneja el servidor
-multicast_addr = '224.0.0.3'
-bind_addr = '0.0.0.0'          
-port = 3000
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
+#multicast_addr = '224.0.0.3'
+#bind_addr = '0.0.0.0'          
+#port = 3000
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
 Main.mainloop()
